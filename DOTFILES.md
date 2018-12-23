9324fa989d8ac00e23ede6297b8e74b590ac8ef2
Maximilian Karacsony <maximilian.karacsony@mail.de>
2018-12-23 16:42:45 +0100

    Add readme and setup script

---
+++ DOTFILES.md
@@ -0,0 +1,18 @@
+# .dotfiles
+This repository contains dotfiles that set up a development environment that
+uses the following tools:
+
+* zsh
+* Visual Studio Code
+
+# Installation
+
+## Clone the repository
+```sh
+git clone --bare git@github.com:Mksony/dotfiles.git $HOME/.cfg
+```
+
+## Setup the config files
+```sh
+bash install.sh
+```
---
+++ install.sh
@@ -0,0 +1,16 @@
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    mkdir -p .config-backup
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
+
+chsh -s `which zsh`
