d65532a4d304b846492293d87ac8a31fa28c7fc5
Max Karacsony <max.karacsony@web.de>
2022-07-04 08:03:46 +0200

    chore: Enable frecency plugin for telescope

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -12,10 +12,12 @@ require("telescope").setup({
 
         mappings = {i = {["<C-x>"] = false, ["<C-q>"] = actions.send_to_qflist}}
     },
-    pickers = {find_files = {find_command = {"fd", "--type", "f", "--strip-cwd-prefix", "--hidden"}}}
+    pickers = {find_files = {find_command = {"fd", "--type", "f", "--strip-cwd-prefix", "--hidden"}}},
     -- }
     -- extensions = {fzy_native = {override_generic_sorter = false, override_file_sorter = true}}
+    extensions = {frecency = {default_workspace = 'CWD', show_scores = true, show_unindexed = true, disable_devicons = false}}
 })
 
 -- require("telescope").load_extension("fzy_native")
+require"telescope".load_extension("frecency")
 require("telescope").load_extension('fzf')
