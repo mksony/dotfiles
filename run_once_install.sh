4de54480778df9849a52ca7799f20e8fa399a922
Max Karacsony <max.karacsony@web.de>
2023-08-02 09:42:24 +0200

    chore: update install script

--- run_once_install.sh
+++ run_once_install.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo "Installing dependencies..."
+echo "Checking if brew is installed..."
+
+# Check if brew is installed, if not install it
+if ! command -v brew &>/dev/null; then
+	echo "Installing Homebrew..."
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+	echo "Homebrew is already installed"
+fi
+
+brew bundle
+
+# to avoid problems setting zsh as default shell
+sudo echo "$(which zsh)" >>/etc/shells
+chsh -s $(which zsh)
+
+bash ~/.macos
