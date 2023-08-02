db38c70ec5e9f0b291c5fe617fcac4d95fd0d68d
Max Karacsony <max.karacsony@web.de>
2023-08-02 08:04:46 +0200

    chore: fix install script

--- scripts/run_once_install.sh
+++ scripts/run_once_install.sh
@@ -1,9 +1,11 @@
+!#/bin/bash
+
 brew bundle
 
 curl -o- -L https://yarnpkg.com/install.sh | bash
 
 # to avoid problems setting zsh as default shell
-sudo echo "$(which zsh)" >> /etc/shells
+sudo echo "$(which zsh)" >>/etc/shells
 chsh -s $(which zsh)
 
 bash ~/.macos
