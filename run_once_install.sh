b17b3688298ee88738c17e4ceb97e8a5eee72e13
Max Karacsony <max.karacsony@web.de>
2023-08-02 10:40:25 +0200

    feat: add install scripts

--- run_once_install.sh
+++ run_once_install.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-echo "Checking if brew is installed..."
-
-# Check if brew is installed, if not install it
-if ! command -v brew &>/dev/null; then
-	echo "Installing Homebrew..."
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-	echo "Homebrew is already installed"
-fi
-
 brew bundle
 
 # to avoid problems setting zsh as default shell
